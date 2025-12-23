1753198d31fc33fca82ca2bc20e362386314df06
João Alves <joao.alves@ecosis.com.br>
2025-12-23 16:06:08 -0300

    create templates folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,5 @@
-import { CallToAction } from '@/components/sections/call-to-action'
-import { CustomerStorySection } from '@/components/sections/customer-story-section'
-import { FeatureSection } from '@/components/sections/feature-section'
-import { HeroSection } from '@/components/sections/hero-section'
-import { SupportSection } from '@/components/sections/support-section'
+import { LandingPage } from '@/templates/landing-page'
 
 export default function Home() {
-  return (
-    <article className="flex flex-col">
-      <HeroSection />
-
-      <FeatureSection />
-
-      <SupportSection />
-
-      <CustomerStorySection />
-
-      <CallToAction />
-    </article>
-  )
+  return <LandingPage />
 }
---
+++ src/templates/landing-page/landing-page.tsx
@@ -0,0 +1,23 @@
+import {
+  CallToAction,
+  CustomerStorySection,
+  FeatureSection,
+  HeroSection,
+  SupportSection,
+} from '@/templates/landing-page/sections'
+
+export function LandingPage() {
+  return (
+    <article className="flex flex-col">
+      <HeroSection />
+
+      <FeatureSection />
+
+      <SupportSection />
+
+      <CustomerStorySection />
+
+      <CallToAction />
+    </article>
+  )
+}
